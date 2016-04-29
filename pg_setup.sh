bf88a116a42694267a39c96c8cf718b91c1568b1
nsas454 <yukimoto@usa-mimi.jp>
2016-04-29 15:28:35 +0900

    セットアップ用のスクリプト

---
+++ pg_setup.sh
@@ -0,0 +1,21 @@
+
+#! /bin/bash
+
+# プログラミング教室用初期セットアップスクリプト
+# pg_setup.shをそのまま実行してください
+# python3専用の開発環境を構築します
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python
+brew install python3
+pip install virtualenv
+pip install virtualenvwrapper
+
+cat <<EOF > ~/.bashrc
+export WORKON_HOME=~/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+EOF
+
+source .bashrc
+mkvirtualenv --python=`which python3` uec
+pip install ipython
